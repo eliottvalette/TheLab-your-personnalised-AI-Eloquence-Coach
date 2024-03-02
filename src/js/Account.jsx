5ad6101b35653ad90d6b7237b8729a1a9f332ee9
Valette Eliott <eliott.valette@hec.edu>
2024-03-03 00:24:59 +0100

    firebase

--- src/js/Account.jsx
+++ src/js/Account.jsx
@@ -1,5 +1,5 @@
 // Laboratoire/src/Account.jsx 
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import {Link} from "react-router-dom"
 import '../css/Account.css'
 
@@ -7,7 +7,9 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword } from "firebase/auth"
+    signInWithEmailAndPassword, 
+    signOut,
+    onAuthStateChanged } from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: "AIzaSyBH4fHeMgD8yY7s6uF3OwWwBEXqlIrPwjQ",
@@ -22,36 +24,40 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 
-function authSignInWithEmail(emailInputEl, passwordInputEl ) {
-    const email = emailInputEl
-    const password = passwordInputEl
-    
+function authSignInWithEmail(emailInputEl, passwordInputEl) {
+    const email = emailInputEl;
+    const password = passwordInputEl;
+  
     signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            console.log('Very Great')
-        })
-        .catch((error) => {
-            console.error(error.message)
-        })
-}
-
-function authCreateAccountWithEmail(emailInputEl, passwordInputEl ) {
-    const email = emailInputEl
-    const password = passwordInputEl
-
+      .then((userCredential) => {
+        console.log('You are now connected to you account');
+        console.log(userCredential)
+      })
+      .catch((error) => {
+        console.error(error.message);
+        alert(error.message); 
+      });
+  }
+  
+  function authCreateAccountWithEmail(emailInputEl, passwordInputEl) {
+    const email = emailInputEl;
+    const password = passwordInputEl;
+  
     createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            console.log('Great')
-        })
-        .catch((error) => {
-            console.error(error.message)
-        })
-}
+      .then((userCredential) => {
+        console.log('Account created');
+        console.log(userCredential)
+      })
+      .catch((error) => {
+        console.error(error.message);
+        alert(error.message); 
+      });
+  }
 
 function authSignOut() {
     signOut(auth)
         .then(() => {
-            console.log('Bye')
+            console.log('Signed Out')
         }).catch((error) => {
             console.error(error.message)
         })
@@ -61,17 +67,40 @@ function authSignOut() {
 export default function Account(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            console.log(isLoggedIn)
+            setIsLoggedIn(!!user);
+        });
+        return () => unsubscribe();
+    }, []);
+
     return(
         <main className="account-main-login">
             <h1 className="account-h1">Mon compte</h1>
-            <form className="account-form-login" id="account-form-login">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" name="email" placeholder="Votre email" onChange={(e)=>setEmail(e.target.value)}/>
-                <label htmlFor="password">Mot de passe</label>
-                <input type="password" id="password" name="password" placeholder="Votre mot de passe" autoComplete="current-password" onChange={(e)=>setPassword(e.target.value)}/>
-                <button className="home-connect" id="home-connect" onClick={authSignInWithEmail(email,password)}>Se connecter</button>
-                <button className="home-create" id="home-create" onClick={authCreateAccountWithEmail(email,password)}>Créer un compte</button>
-            </form>
+            {!isLoggedIn ? 
+                // Login view
+                (<section id="logged-in-view">
+                    <form className="account-form-login" id="account-form-login">
+                        <label htmlFor="email" className="account-label" >Email</label>
+                        <input type="email" className="account-input" id="email" name="email" placeholder="Votre email" onChange={(e)=>setEmail(e.target.value)} autoComplete="username"/>
+                        <label htmlFor="password" className="account-label">Mot de passe</label>
+                        <input type="password" className="account-input" id="password" name="password" placeholder="Votre mot de passe" autoComplete="current-password" onChange={(e)=>setPassword(e.target.value)}/>
+                        <button className="account-btn" id="home-connect" onClick={() => {authSignInWithEmail(email,password)}}>Se connecter</button>
+                        <button className="account-btn" id="home-create" onClick={() => {authCreateAccountWithEmail(email,password)}}>Créer un compte</button>
+                    </form>
+                </section>
+             ) : (
+                // Logout view
+                <section id="logged-out-view">
+                    <h2 className="account-h1">Bienvenue chez vous</h2>
+                    <button className="account-btn" id="home-disconnect" onClick={() => {authSignOut()}}>Se déconnecter</button>f
+                </section>
+             )}
+            
+            
         </main>
         )
 }
