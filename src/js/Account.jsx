6948c07b450a8ce5a336597c431594907e581713
Valette Eliott <eliott.valette@hec.edu>
2024-03-02 17:28:28 +0100

    oo

--- src/js/Account.jsx
+++ src/js/Account.jsx
@@ -3,6 +3,58 @@ import React, { useState } from "react";
 import {Link} from "react-router-dom"
 import '../css/Account.css'
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBH4fHeMgD8yY7s6uF3OwWwBEXqlIrPwjQ",
+  authDomain: "thelab-d1229.firebaseapp.com",
+  projectId: "thelab-d1229",
+  storageBucket: "thelab-d1229.appspot.com",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+function authSignInWithEmail(emailInputEl, passwordInputEl ) {
+    const email = emailInputEl
+    const password = passwordInputEl
+    
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log('Very Great')
+        })
+        .catch((error) => {
+            console.error(error.message)
+        })
+}
+
+function authCreateAccountWithEmail(emailInputEl, passwordInputEl ) {
+    const email = emailInputEl
+    const password = passwordInputEl
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log('Great')
+        })
+        .catch((error) => {
+            console.error(error.message)
+        })
+}
+
+function authSignOut() {
+    signOut(auth)
+        .then(() => {
+            console.log('Bye')
+        }).catch((error) => {
+            console.error(error.message)
+        })
+}
+
+
 export default function Account(){
     return(
         <main className="account-main-login">
@@ -13,10 +65,6 @@ export default function Account(){
                 <label htmlFor="password">Mot de passe</label>
                 <input type="password" id="password" name="password" placeholder="Votre mot de passe"/>
                 <button className="home-submit" id="home-submit">Se connecter</button>
-                {/* <p className="home-p-login">
-                    Mot de passe oublié ? {" "}
-                    <Link to="/reset-password">Cliquez ici</Link>
-                </p> */}
             </form>
         </main>
         )
