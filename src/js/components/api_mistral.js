ec69c9da1bfe03bae01ff783776dc46446da7b4d
Valette Eliott <156651552+eliottvalette@users.noreply.github.com>
2024-09-12 20:17:56 +0200

    prompt fine tuning

--- src/js/components/api_mistral.js
+++ src/js/components/api_mistral.js
@@ -21,11 +21,13 @@ export default async function freeApi(params){
                 {role: 'system', content: 
                 `Analyse des réunions ou entretiens dans le cadre de la gestion d'un grand centre hospitalier régional. La chef de pôle d'oncologie participe à ces échanges, qui portent souvent sur l'organisation hospitalière ou des consultations médicales. 
 
-                <h3>Résumé final détaillé</h3>
-                [Fournis un résumé concis de 2500 charactères environ, en plusieurs paragraphes balisés<p></p> reprenant les éléments essentiels de la discussion, les conclusions tirées et les actions à entreprendre. Fais en sorte que ce résumé soit clair et structuré pour une lecture rapide.]
+                [Résumé détaillé]
+                [Fournis un résumé concis de 2500 charactères environ, en parties contenant plusieurs paragraphes balisés <h3></h3> et<p></p> reprenant les éléments essentiels de la discussion, les conclusions tirées et les actions à entreprendre. Fais en sorte que ce résumé soit clair et structuré pour une lecture rapide.]
+                
                 <h3>Analyse détaillée du contenu de la discussion</h3>
                 [Fournis une évaluation approfondie de la discussion de 500 charactères environ balisé<p></p>, en identifiant les points clés, les décisions prises ou les sujets restés en suspens. Mets en lumière les éléments liés à l'organisation hospitalière, aux décisions médicales, ou à toute interaction entre les acteurs présents. Détaille si nécessaire les points soulevés par les différents intervenants.]
                 <br/>
+                
                 <h3>Évaluation de la qualité des interventions</h3>
                 [Analyse la clarté, la précision et la pertinence des interventions des différents participants, en tenant compte de leur rôle et de l'importance de leur contribution dans le cadre de la discussion. Indique s'il y a eu des divergences, des hésitations ou des problèmes de communication. En 500 charactères environ balisé<p></p>]
                 <br/>
